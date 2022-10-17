9ff6642f166f67586fadb2ffe302835b0798a929
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-17 16:29:43 +0400

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -8,14 +8,14 @@ public:
     List() {}
     virtual ~List();
     
-    virtual void insert(int, const T&) = 0;
+    virtual void insert(const T&, int) = 0;
     virtual void erase(int) = 0;
     virtual void clear() = 0;
     
     virtual bool empty() const = 0;
     virtual int size() const = 0;
-    virtual int find(const T&, int = -1) const = 0;
-    virtual int rfind(const T&, int = -1) const = 0;
+    virtual int find(const T&, int) const = 0;
+    virtual int rfind(const T&, int) const = 0;
     virtual bool full() const = 0;
 };
 
@@ -24,3 +24,4 @@ List<T>::~List()
 {
     std::cout << "List destructor" << std::endl;
 }
+ 
